af417f51f06ab88bddc6c4edb55caf385a75b538
Oleg <alligator2d@rambler.ru>
2023-03-14 08:48:29 +0300

    refactor

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-let clickPrev = document.querySelector('.player-chunk-prev')
-clickPrev.addEventListener('click', prev)
-let clickNext = document.querySelector(('.player-chunk-next'))
-clickNext.addEventListener('click', next)
-function prev() {
-	console.log('left');
+let prev = document.querySelector(".player-chunk-prev");
+let next = document.querySelector(".player-chunk-next");
+
+prev.addEventListener("click", function () {
+	moveClassPrev("timeLine-chunk-active");
+	moveClassPrev("player-chunk-active");
+});
+
+next.addEventListener("click", function () {
+	moveClassNext("timeLine-chunk-active");
+	moveClassNext("player-chunk-active");
+	
+});
+
+function moveClassNext(className) {
+	let active = document.querySelector("." + className);
+	if(active.nextElementSibling) {
+		active.classList.remove(className);
+		active.nextElementSibling.classList.add(className);
+	}
 }
-function next () {
-	console.log('rigth');
+function moveClassPrev(className) {
+	let active = document.querySelector("." + className);
+	let prev = active.previousElementSibling
+	if(prev && (prev.classList.contains('player-chunk') || prev.classList.contains('timeLine-chunk'))) {
+		active.classList.remove(className);
+		active.previousElementSibling.classList.add(className);
+	}
 }
