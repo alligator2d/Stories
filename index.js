2fbf65ed402bb64dbcef6961d1698b629ff42d82
Oleg <alligator2d@rambler.ru>
2023-03-17 08:55:34 +0300

    refactor

---
+++ index.js
@@ -0,0 +1,39 @@
+initPlayer({
+	target: '.my-player',
+	slides:[
+		{
+			url:'https://i.pinimg.com/474x/49/6d/8f/496d8f4a6009aca8bc03131098f3d533.jpg',
+			alt: 'slide1',
+			overlays: [
+				{
+					type: 'text',
+					value: 'Hello!',
+					styles: {
+						style: 'orange',
+						'font-size': '60px',
+						'text-shadow': '1px 1px #000',
+						top: '60%',
+						left: '30%',
+						'transform': 'rotate(-30deg)'
+					}
+				},
+				{
+					type: 'text',
+					value: 'Hello world!',
+					styles: {
+						style: 'blue',
+						'font-size': '30px',
+						'text-shadow': '1px 1px #000',
+						bottom: '60%',
+						right: '30%',
+						'transform': 'rotate(-30deg)'
+					}
+				}
+			]
+		},
+		{url:'https://i.pinimg.com/474x/b7/d5/4c/b7d54cbc102493c40a279a5902eeae5a.jpg', alt: 'slide1'},
+		{url:'https://i.pinimg.com/474x/b0/ea/8b/b0ea8b9f7d4ed652a62b1a58165f1efd.jpg', alt: 'slide1'},
+		{url:'https://i.pinimg.com/474x/ab/57/11/ab571192e42c7700a8e21dee5561091d.jpg', alt: 'slide1'},
+		{url:'https://i.pinimg.com/474x/03/6c/e6/036ce6a861a600547dfce558dcdf5b07.jpg', alt: 'slide1'},
+	],
+})
--- script.js
+++ script.js
@@ -1,20 +1,71 @@
+/**
+ * Инициализирует плеер Stories по заданным параметрам
+ *
+ * @param {{
+ * target: string,
+ * slides: Array<{url: string, alt?: string}>
+ *     }} params
+ * @returns {Element|null}
+ */
+
 function initPlayer(params) {
 	const target = document.querySelector(params.target);
 	if(target === null || params.slides === undefined) {
-		return;
+		return null;
 	}
 	let timeLineChunks = "";
 	let playerChunks = "";
 	let isFirst = true;
+	let timer;
 	
-	for (const el of params.slides) {
-		timeLineChunks += `<div class="timeLine-chunk ${isFirst ? "timeLine-chunk-active" : ""}">
-						<div class="timeLine-chunk-inner"></div></div>`;
-		playerChunks += `<div class="player-chunk ${isFirst ? "player-chunk-active" : ""}">
-						<img src="${el.url}" alt="${el.name || ""}"></div>`;
+	for (const slide of params.slides) {
+		timeLineChunks += generateTimelineChunk(isFirst);
+		playerChunks += generatePlayerChunk(slide, isFirst);
 		isFirst = false;
 	}
-	target.innerHTML = `<div class="player"> 
+	target.innerHTML = generatePlayerLayout();
+	
+	let prev = document.querySelector(".player-chunk-prev");
+	let next = document.querySelector(".player-chunk-next");
+	
+	prev.addEventListener("click", switchToPrev);
+	next.addEventListener("click", switchToNext);
+	
+	runInterval(2, 1);
+	
+	return target.querySelector(".player");
+	
+	function generateTimelineChunk(isFirst) {
+		return `<div class="timeLine-chunk ${isFirst ? "timeLine-chunk-active" : ""}">
+						<div class="timeLine-chunk-inner"></div></div>`;
+	}
+	
+	function generatePlayerChunk(slide, isFirst) {
+		return `<div class="player-chunk ${isFirst ? "player-chunk-active" : ""}">
+						<img src="${slide.url}" alt="${slide.name || ""}"></div>`;
+	}
+	
+	function generateOverlays(slide) {
+		if(slide.overlays === undefined) {
+			return "";
+		}
+		let res = "";
+		for (const el of slide.overlays) {
+			const styles = (el.styles !== undefined ? Object.entries(el.styles) : [])
+			.map((i) => i.join(":")).join(";");
+			
+			res += `<div class="player-chunk-overlay" style="${styles}">el</div>`;
+		}
+		return res;
+		function renderOverlay (overlay) {
+			if (overlay.type === 'text') {
+				return overlay
+			}
+		}
+	}
+	
+	function generatePlayerLayout() {
+		return `<div class="player"> 
 						<div class="timeLine">${timeLineChunks}</div>
 						<div class="player-content-wrapper">
 						<div class="player-chunk-switcher player-chunk-prev"></div>
@@ -22,58 +73,46 @@ function initPlayer(params) {
 						<div class="player-content">${playerChunks}</div>
 						</div>
 						</div>`;
-	
-}
-
-
-let prev = document.querySelector(".player-chunk-prev");
-let next = document.querySelector(".player-chunk-next");
-
-prev.addEventListener("click", function () {
-	const el = moveClass("timeLine-chunk-active", "previousElementSibling");
-	
-	moveClass("player-chunk-active", "previousElementSibling");
-	if(el) {
-		el.querySelector(".timeLine-chunk-inner").style.width = "";
 	}
-});
-
-next.addEventListener("click", nextImg);
-
-function moveClass(className, method) {
-	const active = document.querySelector("." + className);
 	
-	if(active[method]) {
-		active.classList.remove(className);
-		active[method].classList.add(className);
-		return active;
-	}
-	return null;
-}
-
-function nextImg() {
-	const el = moveClass("timeLine-chunk-active", "nextElementSibling");
-	moveClass("player-chunk-active", "nextElementSibling");
-	if(el) {
-		el.querySelector(".timeLine-chunk-inner").style.width = "";
-	}
-}
-
-function runInterval(time, step) {
-	let timer;
-	clearInterval(timer);
-	timer = setInterval(() => {
-		const active = document.querySelector(".timeLine-chunk-active").querySelector(".timeLine-chunk-inner");
-		let w = parseFloat(active.style.width) || 0;
-		if(w === 100 && active) {
-			if(nextImg()) {
-				// active.style.width = "";
-			}
-			return;
+	function moveClass(className, method) {
+		const active = document.querySelector("." + className);
+		
+		if(active[method]) {
+			active.classList.remove(className);
+			active[method].classList.add(className);
+			return active;
 		}
-		active.style.width = String(w + step) + "%";
-	}, time * 1000 * step / 100);
+		return null;
+	}
+	
+	function switchToPrev() {
+		const el = moveClass("timeLine-chunk-active", "previousElementSibling");
+		
+		moveClass("player-chunk-active", "previousElementSibling");
+		if(el) {
+			el.querySelector(".timeLine-chunk-inner").style.width = "";
+		}
+	}
+	
+	function switchToNext() {
+		const el = moveClass("timeLine-chunk-active", "nextElementSibling");
+		moveClass("player-chunk-active", "nextElementSibling");
+		if(el) {
+			el.querySelector(".timeLine-chunk-inner").style.width = "";
+		}
+	}
+	
+	function runInterval(time, step) {
+		clearInterval(timer);
+		timer = setInterval(() => {
+			const active = document.querySelector(".timeLine-chunk-active").querySelector(".timeLine-chunk-inner");
+			let w = parseFloat(active.style.width) || 0;
+			if(w === 100 && active) {
+				switchToNext();
+				return;
+			}
+			active.style.width = String(w + step) + "%";
+		}, time * 1000 * step / 100);
+	}
 }
-
-runInterval(2, 1);
-
